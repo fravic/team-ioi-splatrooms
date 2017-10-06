8b1e7bf9654b170089795b8d226049ff902970ce
Fravic <fravicf@gmail.com>
2017-10-05 23:32:55 -0700

    Add new room asset and GameTimer

---
+++ SplatRooms/Assets/GUI/Scripts/GameTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+	public float secondsInGame;
+	public GameObject textObject;
+
+	float _startTime;
+
+	// Use this for initialization
+	void Start () {
+		StartTimer ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float secondsLeft = secondsInGame - (Time.time - _startTime);
+		System.TimeSpan t = System.TimeSpan.FromSeconds(secondsLeft);
+		textObject.GetComponent<Text>().text = t.ToString ("mm\\:ss\\:ff");
+	}
+
+	public void StartTimer() {
+		_startTime = Time.time;
+	}
+}
