bb46fdea16e4414dc9d0dd96622628588f783ab6
Fravic <fravicf@gmail.com>
2017-10-08 14:47:42 -0700

    Add animated timer

--- SplatRooms/Assets/MainScene/GUI/Scripts/GameTimer.cs
+++ SplatRooms/Assets/MainScene/GUI/Scripts/GameTimer.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class GameTimer : MonoBehaviour {
 
 	public UnityEvent onTimerComplete;
 	public float secondsInGame;
 	public GameObject textObject;
 
+	CanvasGroup _canvasGroup;
 	bool _endEventFired = false;
 	float _startTime;
 
 	// Use this for initialization
 	void Start () {
+		_canvasGroup = GetComponent<CanvasGroup>();
 		StartTimer ();
 	}
 
@@ -23,7 +26,15 @@ public class GameTimer : MonoBehaviour {
 	void Update () {
 		float secondsLeft = Math.Max(secondsInGame - (Time.time - _startTime), 0);
 		System.TimeSpan t = System.TimeSpan.FromSeconds(secondsLeft);
-		textObject.GetComponent<Text>().text = t.ToString ("mm\\:ss\\:ff");
+		textObject.GetComponent<Text>().text = t.ToString ("mm\\:ss");
+
+		// Flash thrice every 10s or every second during the last 10
+		int floorSec = Mathf.FloorToInt(secondsLeft);
+		if (floorSec % 10 == 0 || floorSec % 10 == 9 || floorSec % 10 == 8 || floorSec < 10) {
+			_canvasGroup.alpha = 1 - (Mathf.Ceil(secondsLeft) - secondsLeft);
+		} else {
+			_canvasGroup.alpha = 0;
+		}
 
 		if (secondsLeft <= 0 && !_endEventFired) {
 			onTimerComplete.Invoke();
